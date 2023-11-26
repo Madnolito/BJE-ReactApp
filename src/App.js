ad84d67da3c24427c5e982556a0913a5eea1980a
JoakoJC <joakocortes123@gmail.com>
2023-11-26 20:31:04 -0300

    primer commit joaquito

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         HOLA BENJA CULIAO<code>src/App.js</code> and save to reload.
+         HOLA MANU CULIAO<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
