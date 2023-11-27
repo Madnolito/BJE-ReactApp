6bd5f78162adf15364224680b9bb56b5a440d161
Emanuel Tapia <emanuel.tapia.ext@telefonica.com>
2023-11-26 21:59:20 -0300

    Estructura de Archivos

---
+++ src/components/First-component.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function FirstComponent() {
+  return (
+    <div className="App">
+        <p>
+        Primer Componente
+        </p>
+    </div>
+  );
+}
+
+export default FirstComponent;
---
+++ src/pages/First-page.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function FirstPage() {
+  return (
+    <div className="App">
+        <p>
+        Primera Page 
+        </p>
+    </div>
+  );
+}
+
+export default FirstPage;
