dc9ccf4c512b3b6bb1d436710ca368b88f700f50
JoakoJC <joakocortes123@gmail.com>
2023-11-26 22:15:58 -0300

    manu imagen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import semen from './assets/images/manu-pa-la-caga.jpeg' ;
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={semen} className="App-logo" alt="logo" />
         <p>
         {/* Se recibe en Index.js linea 10, se define por "root" linea 7, y esto se inserta en index.html linea 31  */}
         Primer componente 
