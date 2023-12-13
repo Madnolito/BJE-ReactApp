601a458144e8b4786642d3ba30fd4a8139967f9d
Emanuel Tapia <emanuel.tapia.ext@telefonica.com>
2023-12-13 13:03:31 -0300

    Fix: react bug warning

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="Container">
       <Header />
       <Routes>
-           <Route path='/' element={<Home />} /> {/*Se muestra el home cuando no coincida con los otros '/' path */}
+          <Route path='/' element={<Home />} /> {/*Se muestra el home cuando no coincida con los otros '/' path */}
           <Route path='/benja' element={<Benja/>}/>
           <Route path='/joakito' element={<Joakitowo/>}/>
           <Route path='manu' element={ <Manu/>} />
